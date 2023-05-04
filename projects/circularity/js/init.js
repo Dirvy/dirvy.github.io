2d7ca302a2d0a291ee99d0fb3adce6c2eb7bf55e
Ivy/Dirk <delusional20creativity@gmail.com>
2023-05-04 22:25:59 +0000

    meow

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -25,7 +25,7 @@ var circles = [];
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -52,7 +52,11 @@ var circles = [];
             physikz.updatePosition(circles[3]);
             physikz.updatePosition(circles[4]);
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
            
